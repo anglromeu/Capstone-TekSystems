7f82cec2ea8f0d635b1365f4cf199e72026423d6
ANGEL ROMEU <104474288+anglromeu@users.noreply.github.com>
2022-07-18 14:41:31 +0000

    Added input to read xlsx file moved the conversion to csv higher

--- main.py
+++ main.py
@@ -1,68 +1,31 @@
 #!/usr/bin/env python3
 
-import openpyxl
 import pandas as pd
 import xlsxwriter
-import xlrd
 import os
 import csv
+import re
+import random
+from time import sleep
+import xlswriter
 
 
 #main(excel sheet)
   #Output a sorted excel sheet by platoons (groups), using define parameters (age, sex)
-def main():
-#Read excel file from generated report
-  original = pd.read_excel('list_10.xlsx')
-#print (original)
+# Create a list for each row
+def make_list():
+  file_name = input('Enter the xlsx file you want to work with: ')
+  # Take xlsx data name, convert to csv to be used in our program
+  read_file = pd.read_excel(f"{file_name}.xlsx")
 
-#Convert excel to csv
-  original.to_csv('list_10.csv',
+  read_file.to_csv(f"{file_name}.csv",
                 index = None,
-                header = True)
-  original_csv = pd.DataFrame(pd.read_csv('list_10.csv'))
-#print(original_csv)
-
-#Create a one list for each row
-  with open("list_10.csv") as f:
-    reader = csv.reader(f)
-    original_list = list(reader)
-#print(original_list)
-
-#Ask user for number of "platoons"
-  platoons = int(input('How many Basic Training Platoons?\n'))
-
-#Creat list for each age group
-  #thirteen = 13-14
-  #fifteen = 15-16
-  #seventeen = 17
-  thirteen = []
-  fifteen = []
-  seventeen = []
-
-
-#Search age column and add each line of infromation from exel to appropriate age group
-  #use regex to achive this
-  #loop can be use here
-  #exit loop after all names on excel sheet are assigned to lists
-  #chance to use a while loop
-
-#Search each list for females using regex
-  #assign females to a new list
-  #if element can be use here
-
-#Iterate through each age group list and randomly assign to each platoon list
-  #function should exit once age group and female list are empty
-  #considering random.sample()
-  #each platoon list should have an aproximate number of people (evenly distrubuted prerred)
-
-#Search female list and add to appropriate age group list (lists should be empty now)
-
-#Iterate through each age group list and randomly assign to each platoon list
-
-#Create a new excel sheet with platoon assignments
-  #Considering xlswriter
-  #ideally sheet should alphabetize by last name
-  #save excel sheet in the same folder as the original
+                header=True)
+  # Open and read the csv 
+  with open(f"{file_name}.csv") as f:
+      reader = csv.reader(f)
+      original_list = list(reader)
+  return original_list
 
 
 
