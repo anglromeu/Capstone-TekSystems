d9cb8741dc1a4f856e26cee1af5033dc522044f8
ANGEL ROMEU <104474288+anglromeu@users.noreply.github.com>
2022-07-19 18:35:55 +0000

    test_project

---
+++ test_project.py
@@ -0,0 +1,13 @@
+import pytest
+
+def divide_into_platoons(thirteen, fifteen, seventeen, females):
+    max_list = [platoon_first, platoon_second, platoon_third, platoon_fourth,     platoon_fifth, platoon_sixth, platoon_seventh, platoon_eight]
+    assert type(max_list) is list
+
+def female_count():
+    if int(female_bunks) > 100:
+      raise TypeError('Maximum capacity of 100 exceeded! ')
+    
+def make_list():
+    with open(f"{file_name}.csv") as f:
+        assert type(f) is list
